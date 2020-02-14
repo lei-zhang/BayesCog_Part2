6021c7408e8f29c50ceee5c5c578a059b04feaa8
lei-zhang <zhang.ninhamburg@gmail.com>
2020-02-14 12:19:45 +0100

    update

--- 06.reinforcement_learning/_scripts/simu_rw_demo.R
+++ 06.reinforcement_learning/_scripts/simu_rw_demo.R
@@ -1,4 +1,33 @@
-# simulate one participant's data using Rescorla-Wagner Rl model
+# simulate data using Rescorla-Wagner Rl model
+
+#### =========================================================
+# solution for simulating multiple participants
+#### =========================================================
+alpha_mu = 0.3 
+tau_mu = 2 
+
+alpha_sd = 0.3
+tau_sd = 1.2
+
+ns = 10
+
+tmp = 1
+
+while ( tmp ) {
+    alpha = rnorm(ns, alpha_mu, alpha_sd)
+    if (sum(alpha < 0) == 0 & sum(alpha > 1)==0 ) { tmp = 0 }
+}
+
+tmp1 = 1
+while ( tmp1 ) {
+    tau = rnorm(ns, tau_mu, tau_sd)
+    if (sum(tau < 0) == 0 & sum(tau > 5)==0 ) { tmp1 = 0 }
+}
+
+
+#### =========================================================
+# simulation for one person
+#### =========================================================
 
 alpha = 0.3 # learning rate
 tau = 2     # inverse temperature
@@ -30,6 +59,9 @@ rl_ss[,1] = c
 rl_ss[,2] = r
 
 save(rl_ss, file = '_data/rl_sp_ss_simulation_demo.RData')
+
+
+
 
 
 
